c9b4d7e8a4dfadca3a8fb04734589e846396cc59
Lone-wolfRUS <wolf93490@gmail.com>
2025-12-04 15:59:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-### Задача 1
+// Задача 1
 
 function concatenateStrings(str1, str2) {
     return (str1 + str2).replace(/ /g, '');
 }
 
-### Задача 2
+// Задача 2
 
 function getStringLength(str) {
     return str.length;
 }
 
-### Задача 3
+// Задача 3
 
 function getStringFromTemplate(firstName, lastName) {
     return `Hello, ${firstName} ${lastName}!`;
 }
 
 
-### Задача 4
+// Задача 4
 
 function getChar(str, index) {
     return str[index - 1];
 }
 
-### Задача 5
+// Задача 5
 
 function removeFirstOccurrences(str, subStr) {
     return str.replace(subStr, '');
 }
 
-### Задача 6
+// Задача 6
 
 function getRectangleString(width, height) {
     const line = '*'.repeat(width);
