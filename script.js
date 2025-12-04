2e2c4ad79925a7da6feebe7389d17776fc77df2c
Lone-wolfRUS <wolf93490@gmail.com>
2025-12-04 15:27:16 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+### Задача 1
+
+function concatenateStrings(str1, str2) {
+    return (str1 + str2).replace(/ /g, '');
+}
+
+### Задача 2
+
+function getStringLength(str) {
+    return str.length;
+}
+
+### Задача 3
+
+function getStringFromTemplate(firstName, lastName) {
+    return `Hello, ${firstName} ${lastName}!`;
+}
+
+
+### Задача 4
+
+function getChar(str, index) {
+    return str[index - 1];
+}
+
+### Задача 5
+
+function removeFirstOccurrences(str, subStr) {
+    return str.replace(subStr, '');
+}
+
+### Задача 6
+
+function getRectangleString(width, height) {
+    const line = '*'.repeat(width);
+    return (line + '\n').repeat(height).trim();
+}
